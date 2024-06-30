e88f5e9daed0cd418230e2ef03534758a9cdd937
collideinscope <collide.in.scope4@gmail.com>
2024-06-30 14:44:10 +0400

    updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import styles from './styles.module.css';
 
 import logoSymbol from '../../assets/blue-x.svg'; 
+import { NAV_MENU } from '../../constants';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
-  
+  const [activeMenu, setActiveMenu] = useState(null);
+
+  const handleMouseEnter = (menu) => {
+    setActiveMenu(menu);
+  };
+
+  const handleMouseLeave = () => {
+    setActiveMenu(null);
+  };
+
   const generateLogo = () => {
     return (
       <h1>
@@ -14,27 +29,75 @@ const Header = () => {
           className={styles.logo} 
         />
       </h1>
-    )
-  }
+    );
+  };
 
   const generateNav = () => {
+
+    const navMenuItems = Object
+      .keys(NAV_MENU)
+      .map((menu) => {
+
+        const subNavItem = (
+          <ul 
+            className={styles.subNav}
+          >
+            {Object.keys(NAV_MENU[menu]).map((subMenu) => {
+              return (
+                <li className={styles.subNavItem}>
+                  <Link 
+                    key={subMenu} 
+                    to={NAV_MENU[menu][subMenu].route} 
+                    className={styles.subNavItem}
+                  >
+                    <span>{subMenu}</span>
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        )
+
+        const renderSubNavItem = activeMenu === menu
+          ? subNavItem 
+          : '';
+
+        const activeNavItemClass = activeMenu === menu 
+          ? 'activeNav'
+          : '';
+
+        return (
+          <li
+            key={menu} 
+            className={`${styles.navItem} ${activeNavItemClass}`}
+            onMouseEnter={() => handleMouseEnter(menu)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <span>{menu}</span>
+            {renderSubNavItem}
+          </li>
+        )
+      })
+
     return (
-      <nav>
-        nav
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          {navMenuItems}
+        </ul>
       </nav>
-    )
-  }
+    );
+  };
 
   const generateSearch = () => {
     return (
-      <section className={styles['search-container']}>
-        search
+      <section className={styles.searchContainer}>
+        <FontAwesomeIcon className={styles['fa-magnifying-glass']} icon={faMagnifyingGlass} />
       </section>
-    )
+    );
   }
 
   return (
-    <header>
+    <header className={styles.header}>
       {generateLogo()}
       {generateNav()}
       {generateSearch()}
---
+++ src/constants/index.js
@@ -0,0 +1,38 @@
+const NAV_MENU = {
+  Core: {
+    coreA: {
+      route: '',
+    },
+    coreB: {
+      route: '',
+    }
+  },
+  Perps: {
+    perpsA: {
+      route: '',
+    },
+    perpsB: {
+      route: '',
+    }
+  },
+  Stakers: {
+    stakersA: {
+      route: '',
+    },
+    stakersB: {
+      route: '',
+    }
+  },
+  Traders: {
+    tradersA: {
+      route: '',
+    },
+    tradersB: {
+      route: '',
+    }
+  },
+}
+
+export {
+  NAV_MENU,
+}
