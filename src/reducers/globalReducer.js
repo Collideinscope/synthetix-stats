8bf7f401f41fc7ed00a6283b9c15e729a69ecb40
collideinscope <collide.in.scope4@gmail.com>
2024-08-22 18:48:01 +0800

    updated

--- src/reducers/globalReducer.js
+++ src/reducers/globalReducer.js
@@ -5,22 +5,16 @@ export const globalInitialState = getInitialState();
 export const globalReducer = (state = globalInitialState, action) => {
   switch (action.type) {
     case 'SET_INITIAL_DATA':
-      console.log('initial', action.payload)
-      
       return {
         ...state,
         ...action.payload,
       };
     case 'SET_PAGE_DATA':
-      console.log('page', action.payload)
-
       return {
         ...state,
         ...action.payload,
       };
     case 'SET_ALL_REMAINING_DATA':
-      console.log('remaining', action.payload)
-
       return {
         ...state,
         ...action.payload,
